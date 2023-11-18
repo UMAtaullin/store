e7f0d724ab0c715f6489984b21321ab1029bbe64
Ural Ataullin <ataullinural@gmail.com>
2023-11-18 20:42:31 +0500

    add app products

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent   # Для определения корневого каталога.
 
 SECRET_KEY = 'django-insecure-4au3(lk_rk@($uap^$^p+ggoh#m75dwoto&!e4=g!uy^0iku-n'
 
@@ -8,22 +8,33 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 INSTALLED_APPS = [
+    # Управлять данными приложения: добавление, удаление и изменение записей в БД.
     'django.contrib.admin',
+    # Oбеспечивает систему аутентификации и авторизации: управление пользователями,
+    # группами и привилегиями, а также предоставляет механизмы аутентификации,
+    # такие как вход через логин/пароль или социальные сети.
     'django.contrib.auth',
+    # Позволяет создавать связи между моделями
     'django.contrib.contenttypes',
+    # Предоставляет поддержку сессий для веб-приложений, позволяет хранить данные сеанса
+    # для каждого пользователя, обеспечивая сохранение состояния между запросами.
     'django.contrib.sessions',
+    # Управления сообщениями обратной связи с пользователями.
     'django.contrib.messages',
+    # Организации и обслуживании статических файлов проекта.
     'django.contrib.staticfiles',
+
+    # local apps
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.security.SecurityMiddleware',            # Безопасность.
+    'django.contrib.sessions.middleware.SessionMiddleware',     # Проброска сессий для пользователей.
+    'django.middleware.common.CommonMiddleware',                # ...
+    'django.middleware.csrf.CsrfViewMiddleware',                # Защита от атак csrf токены.
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # Аунтификацию добавляют.
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -32,7 +43,7 @@ ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',   # Движок для шаблонов.
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -57,7 +68,7 @@ DATABASES = {
 }
 
 
-AUTH_PASSWORD_VALIDATORS = [
+AUTH_PASSWORD_VALIDATORS = [    # Валидация для паролей. По умолчанию их 4.
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -82,6 +93,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # Это все стили, скрипты, изображения.
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
