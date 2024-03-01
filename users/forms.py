c5be8fa1bed49571f08b8e18d6c0358e21de26e6
Ural Ataullin <ataullinural@gmail.com>
2024-03-01 16:10:47 +0500

    DRY for registration.html

--- users/forms.py
+++ users/forms.py
@@ -16,18 +16,24 @@ class UserLoginForm(AuthenticationForm):
 
 
 class UserRegistrationForm(UserCreationForm):
-    first_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
-    last_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите фамилию'}))
-    username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
-    email = forms.CharField(widget=forms.EmailInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
+    first_name = forms.CharField(
+        label='Имя', widget=forms.TextInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Введите имя'}))
+    last_name = forms.CharField(
+        label='Фамилия', widget=forms.TextInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Введите Фамилию'}))
+    username = forms.CharField(
+        label='Имя пользователя', widget=forms.TextInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+    email = forms.CharField(
+        label='Адрес электронной почты', widget=forms.EmailInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Введите адрес эл. почты'}))
+    password1 = forms.CharField(
+        label='Пароль', widget=forms.PasswordInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+    password2 = forms.CharField(
+        label='Подтверждение пароля', widget=forms.PasswordInput(attrs={
+            'class': 'form-control py-4', 'placeholder': 'Подтвердите пароль'}))
 
     class Meta:
         model = User
